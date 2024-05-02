74e21306c595c121369e6f29ce915862b1cee608
K SANJU KUMAR <129849456+sanjukumarkanki@users.noreply.github.com>
2024-05-02 12:23:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -473,7 +473,7 @@ app.get("/day-wise-followups/:date", async (req,res) => {
     try{
         const fetchDetails = await executeQuery(`SELECT 
         allleads.id,allleads.callerName,  allleads.patientName, allleads.stage, followup_table.coachNotes,followup_table.followupId, followup_table.followupId,followup_table.date
-        FROM ciondatabase.allleads
+        FROM allleads
         INNER JOIN followup_table ON allleads.id = followup_table.leadId
         WHERE  followup_table.date = '${date}'  AND followup_table.status != "Cancelled" AND followup_table.status != "Done" AND allleads.level != "closed"
         ORDER BY allleads.id DESC
