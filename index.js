2779ab86a6ecb62603c48156a62c9f8c4ae6943c
K SANJU KUMAR <129849456+sanjukumarkanki@users.noreply.github.com>
2024-05-02 10:10:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require("cors")
-const mysql = require('mysql2');
+const mysql = require('mysql');
 
 const cron = require('node-cron');
 const { addDays, format, isSunday } = require("date-fns");
